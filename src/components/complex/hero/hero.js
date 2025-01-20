901156f5fddd0c68f0897c37112259b83fd83eff
AlexanderBirg <alexander.birg@yandex.ru>
2025-01-20 20:23:51 +0400

    Верстка первых секций + шрифты

---
+++ src/components/complex/hero/hero.js
@@ -0,0 +1,8 @@
+/* begin hero */
+document.addEventListener('DOMContentLoaded', () => {
+  const heroVideo = document.querySelector('.hero__video');
+  if (!heroVideo) return;
+
+  heroVideo.classList.add('hero__video--show');
+});
+/* end hero */
--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,10 @@
 /* begin scripts */
-/* import './components/complex/...'; */
+import LazyLoad from 'vanilla-lazyload';
+
+import './components/complex/hero/hero.js';
+
+new LazyLoad({
+  elements_selector: '.js-lazy',
+  threshold: 1000,
+});
 /* end scripts */
